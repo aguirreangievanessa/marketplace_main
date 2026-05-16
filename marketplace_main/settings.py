9459902cae21b1daad7672856ebdbf51bbdf8f33
LopezOmar2027 <omar.universe16@gmail.com>
2026-05-15 19:06:29 -0700

    Revert "SPRINT 1"
    This reverts commit 5176d1cb907e09c4963f9074a4bf2ef4da079ce7.

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
 ]
-
-AUTH_USER_MODEL = 'store.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
